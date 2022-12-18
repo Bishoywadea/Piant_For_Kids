c557440d8eb5161625d07f9736bcaf1a4eb6841f
Bishoywadea <108888519+Bishoywadea@users.noreply.github.com>
2022-12-19 00:00:08 +0200

    const error edited

--- Figures/CRectangle.h
+++ Figures/CRectangle.h
@@ -12,7 +12,7 @@ public:
 	CRectangle(Point , Point, GfxInfo FigureGfxInfo );
 	virtual void Draw(Output* pOut) const;
 	void PrintInfo(Output* pOut);
-	virtual bool IsOnFig(int x, int y) const;
+	virtual bool IsOnFig(int x, int y) ;
 	void MOVE(Point p1);
 };
 
